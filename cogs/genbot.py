1d124fc072d49011b2468f79c54dbeb421a89674
CinnamonSea2073 <Keimonn0329@gmail.com>
2022-09-07 00:38:55 +0900

    日替わり秘境コマンド実装

--- cogs/genbot.py
+++ cogs/genbot.py
@@ -64,13 +64,11 @@ class DayOfWeekUnexploredRegion:
         # embedの追加
         embed = discord.Embed(
             title=f"{day_of_week}の日替わり秘境はこちら", color=0x1e90ff)
-        embed.set_image(url="attachment://hoge.png")
+        embed.set_image(url=url)
         DayOfWeekUnexploredRegion.EMBEDS[key] = embed
         # optionsの追加
         DayOfWeekUnexploredRegion.SELECT_OPTIONS.append(
             discord.SelectOption(label=day_of_week, value=key))
-        # ファイルの追加
-        DayOfWeekUnexploredRegion.FILES[key] = discord.File(url, "image.png")
 
 
 y = yaml("weekday.yaml")
@@ -83,17 +81,18 @@ for k, v in _DATA.items():
 
 class weekselectView(View):
     def __init__(self):
-        self.weekday = datetime.date.today().weekday()
+        self.weekday = str(datetime.date.today().weekday())
+        super().__init__()
 
     @discord.ui.button(label="今日の秘境に戻る")
     async def today(self, _: discord.ui.Button, interaction: discord.Interaction):
-        self.weekday = datetime.date.today().weekday()
-        await interaction.response.edit_message(embed=DayOfWeekUnexploredRegion.EMBEDS[self.weekday], file=DayOfWeekUnexploredRegion.FILES[self.weekday], view=self)
+        self.weekday = str(datetime.date.today().weekday())
+        await interaction.response.edit_message(embed=DayOfWeekUnexploredRegion.EMBEDS[self.weekday], view=self)
 
     @discord.ui.button(label="次の日の秘境")
     async def nextday(self, _: discord.ui.Button, interaction: discord.Interaction):
-        self.weekday += 1
-        await interaction.response.edit_message(embed=DayOfWeekUnexploredRegion.EMBEDS[self.weekday], file=DayOfWeekUnexploredRegion.FILES[self.weekday], view=self)
+        self.weekday = str((int(self.weekday) + 1) % 7)
+        await interaction.response.edit_message(embed=DayOfWeekUnexploredRegion.EMBEDS[self.weekday], view=self)
 
     @discord.ui.select(
         placeholder="確認したい曜日を選択",
@@ -104,24 +103,8 @@ class weekselectView(View):
         view = self
         print(
             f"実行者:{interaction.user.name}\n鯖名:{interaction.guild.name}\n日替わり - {select.values[0]}")
-        await interaction.response.edit_message(embed=DayOfWeekUnexploredRegion.EMBEDS[value], file=DayOfWeekUnexploredRegion.FILES[value], view=view)
+        await interaction.response.edit_message(embed=DayOfWeekUnexploredRegion.EMBEDS[value], view=view)
 
-async def getDatatime(hoge):
-        if hoge == 0:
-            hoge = discord.File("C:\\Users\\Cinnamon\\Desktop\\DebugGenshinNetwork\\Image\\today\\Monday.png", f"hoge.png")
-        if hoge == 1:
-            hoge = discord.File("C:\\Users\\Cinnamon\\Desktop\\DebugGenshinNetwork\\Image\\today\\Tuesday.png", f"hoge.png")
-        if hoge == 2:
-            hoge = discord.File("C:\\Users\\Cinnamon\\Desktop\\DebugGenshinNetwork\\Image\\today\\Wednesday.png", f"hoge.png")
-        if hoge == 3:
-            hoge = discord.File("C:\\Users\\Cinnamon\\Desktop\\DebugGenshinNetwork\\Image\\today\\Thursday.png", f"hoge.png")
-        if hoge == 4:
-            hoge = discord.File("C:\\Users\\Cinnamon\\Desktop\\DebugGenshinNetwork\\Image\\today\\Friday.png", f"hoge.png")
-        if hoge == 5:
-            hoge = discord.File("C:\\Users\\Cinnamon\\Desktop\\DebugGenshinNetwork\\Image\\today\\Saturday.png", f"hoge.png")
-        if hoge == 6:
-            hoge = discord.File("C:\\Users\\Cinnamon\\Desktop\\DebugGenshinNetwork\\Image\\today\\Sunday.png", f"hoge.png")
-        return hoge
 
 class GenbotCog(commands.Cog):
 
@@ -133,22 +116,22 @@ class GenbotCog(commands.Cog):
 
     @genbot.command(name='help', description='原神ステータスbotに困ったらまずはこれ！')
     async def chelp(self, ctx):
-        embed = discord.Embed(title=f"helpコマンド：メインコマンド",color=0x1e90ff)
+        embed = discord.Embed(title=f"helpコマンド：メインコマンド", color=0x1e90ff)
         embed.add_field(
             name=f"このbotのメインとなるコマンドです。",
             value=f"\
                 \n**・/genshinstat get**\n自分以外が見ることができない状態で原神のステータスを取得します。UIDリスト機能で、自分のUIDを登録しておくと簡単に使えます。原神の設定でキャラ詳細を公開にすると、キャラステータスも確認できます。\
         ")
         view = helpselectView(timeout=None)
-        await ctx.respond("確認したいコマンドのジャンルを選択してね",embed=embed,view=view,ephemeral=True)  # レスポンスで定義したボタンを返す
+        # レスポンスで定義したボタンを返す
+        await ctx.respond("確認したいコマンドのジャンルを選択してね", embed=embed, view=view, ephemeral=True)
 
     @genbot.command(name='today', description='今日の日替わり秘境などをまとめて確認！')
     async def today(self, ctx):
-        today = datetime.date.today()
-        hoge = today.weekday()
-        embed = discord.Embed(title=f"本日の日替わり秘境はこちら！",color=0x1e90ff)
-        embed.set_image(url=f"attachment://hoge.png") 
-        await ctx.respond(embed=embed,file=await getDatatime(hoge),view=weekselectView(),ephemeral=True)
+
+        view = weekselectView()
+        weekday = view.weekday
+        await ctx.respond(embed=DayOfWeekUnexploredRegion.EMBEDS[weekday], view=view, ephemeral=True)
 
 def setup(bot):
     bot.add_cog(GenbotCog(bot))
