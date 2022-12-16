11555c1e06488beaef9c8a3368f0532d58380078
CinnamonSea2073 <Keimonn0329@gmail.com>
2022-12-17 00:50:06 +0900

    権限不足のエラー表示追加

--- cogs/setting.py
+++ cogs/setting.py
@@ -26,12 +26,23 @@ class SettingCog(commands.Cog):
                                   description="通知を送るチャンネル")):
 
         channel: discord.TextChannel = channel
-        notification.set_notification_channel(ctx.guild_id, channel.id)
-
         embed = discord.Embed(title="通知をこちらのチャンネルから送信します", color=0x1e90ff,
                               description=f"サーバー名：{ctx.guild.name}\nチャンネル名：{channel.name}")
-        channel = self.bot.get_partial_messageable(channel.id)
-        await channel.send(embed=embed)
+        messageble_channel = self.bot.get_partial_messageable(channel.id)
+        try:
+            await messageble_channel.send(embed=embed)
+        except discord.errors.Forbidden:
+            embed = discord.Embed(title="⚠エラー", color=0x1e90ff,
+                                  description=f"該当チャンネルではbotの権限が足りません。\nチャンネルの設定から、下記の画像のような項目で**botにメッセージを送信する権限が与えられているか**確認してください。")
+            embed.add_field(name="必要な権限", value="チャンネルを見る、メッセージを送信")
+            embed.add_field(name="権限不足のチャンネル", value=f"<#{channel.id}>")
+            embed.set_image(
+                url="https://cdn.discordapp.com/attachments/1021082211618930801/1053331845724516402/image.png")
+            await ctx.respond(embed=embed, ephemeral=True)
+            print(
+                f"\n実行者:{ctx.user.name}\n鯖名:{ctx.guild.name}\nsetting_channel - Fordidden_set")
+            return
+        notification.set_notification_channel(ctx.guild_id, channel.id)
         await ctx.respond(content="設定しました。", ephemeral=True)
         print(
             f"\n実行者:{ctx.user.name}\n鯖名:{ctx.guild.name}\nsetting_channel - set")
