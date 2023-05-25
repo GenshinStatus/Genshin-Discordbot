6a55584b7ff198a8b8139cfc3f4c53e828a30799
nikawamikan <wkwktktkkrkr@outlook.jp>
2023-05-25 19:08:50 +0900

    画像ファイルの更新

---
+++ lib/enkaconnecter.py
@@ -0,0 +1,22 @@
+import aiohttp
+from lib.yamlutil import yaml
+import redis
+import json
+
+pool = redis.ConnectionPool(host="redis")
+redis_obj = redis.StrictRedis(connection_pool=pool)
+
+
+async def get_data(uid) -> dict:
+    if redis_obj.keys(uid):
+        print("キャッシュ返してるよー")
+        resp = json.loads(redis_obj.get(uid))
+    else:
+        url = f"https://enka.network/api/uid/{uid}"
+        async with aiohttp.ClientSession(raise_for_status=True, headers={"User-Agent": "Genshin-Discordbot/1.6.7 (Linux; Ubuntu22.04; discord:CinnamonSea2073.zip#2073)"}) as session:
+            async with session.get(url) as response:
+                resp = await response.json()
+                redis_obj.set(uid, json.dumps(resp))
+                redis_obj.expire(uid, 300)
+                print("新規だよー")
+    return resp
