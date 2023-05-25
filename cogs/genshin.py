931721bd3363f25d1246c44b6f9a3d344928f11e
nikawamikan <wkwktktkkrkr@outlook.jp>
2023-05-25 23:35:38 +0000

    エラーメッセージの変更

--- cogs/genshin.py
+++ cogs/genshin.py
@@ -17,6 +17,7 @@ from lib.log_output import log_output, log_output_interaction
 from model.genshin_model import GenshinUID
 import view.genshin_view as genshin_view
 from aiohttp import client_exceptions
+
 async def get_profile(uid, interaction: discord.Interaction):
     embed = genshin_view.LoadingEmbed(description='プロフィールをロード中...')
     await interaction.response.edit_message(content=None, embed=embed, view=None)
@@ -27,12 +28,12 @@ async def get_profile(uid, interaction: discord.Interaction):
         status_code = e.status
 
         messages ={
-            400: "UIDのフォーマットが間違っています。\nフォーマットがあっているかを試してみてください",
+            400: "UIDのフォーマットが間違っています。\n半角数字で入力してください",
             404: "入力されたものが存在するUIDではありません\nもう一度確認してやり直してください。",
-            424: "ゲームメンテナンスやアップデートの影響により\nデータ取得もとのEnka.networkがデータを返却していない状態です。\nこのエラーはしばらく時間をおいてから試すと回復している可能性が高いです。",
-            429: "リミットレートの制限に引っかかっています。\n開発者に対してコンタクトをとってください。",
-            500: "データ取得もとのEnkaNetWorkのサーバーにエラーが発生しています。\n開発者もお手上げかもしれないです。",
-            503: "データ取得もとのEnkaNetWorkサーバーの一時停止中です。\n一時なので恐らく復旧します。（開発者はこれについて確認ぐらいしか取れないです）"
+            424: "ゲームメンテナンスやアップデートの影響により\nEnka.network（ビルドデータを取得するサービス）が停止している状態です。\nこのエラーはしばらく時間をおいてから試すと回復している可能性が高いです。",
+            429: "処理が追いついていません。\nしばらくしても解決しない場合は、開発者に対してコンタクトをとってください。",
+            500: "Enka.network（ビルドデータを取得するサービス）のサーバーにエラーが発生しています。\n詳しくはEnkaのTwitterを確認してください。https://twitter.com/EnkaNetwork",
+            503: "Enka.network（ビルドデータを取得するサービス）サーバーの一時停止中です。\nしばらくお待ちください。※開発者はこれについて確認ぐらいしか取れないです。\n詳しくはEnkaのTwitterを確認してください"
         }
         embed = genshin_view.ErrorEmbed(
             description=messages[status_code])
